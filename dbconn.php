cd32e196bd3fba5180c369725f020cf3781f78d2
Jinsoo Yoon <decinc@naver.com>
2013-04-01 01:05:24 +0900

    asd

--- dbconn.php
+++ dbconn.php
@@ -3,7 +3,7 @@ $hostname = "tcp:skugbj1ioa.database.windows.net,1433"; //서버 ip
 $username = "decinc@skugbj1ioa";  //db접근 user
 $password = "dbswlstn1!";  //db접근 user password
 $dbName = "decinc";  //odbc dns명
- 
+ sqlsrv_configure('WarningsReturnAsErrors', 0);
 $conn = sqlsrv_connect($hostname, array("UID"=>$username, "PWD"=>$password, "Database"=>$dbName));
 
 if($conn === false){
