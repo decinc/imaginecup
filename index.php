d112416b8300b7a37a56b4c3f4aeec87fb45b298
Jinsoo Yoon <decinc@naver.com>
2013-04-01 00:48:12 +0900

    dfg

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "dbconn.php";
 $stmt = sqlsrv_query("select * from user");
   if ($stmt === false)
    {
-     FatalError("Failed to query test table: ");
+     die("Failed to query test table: ");
    }
 while($row = sqlsrv_fetch_array($stmt)){
  print_r($row);
