f1b1ebad089dfea38412caa9c69005008527236a
Jinsoo Yoon <decinc@naver.com>
2013-04-01 00:35:38 +0900

    lk

--- dbconn.php
+++ dbconn.php
@@ -1,15 +1,14 @@
 <?php
-$hostname = "skugbj1ioa.database.windows.net"; //서버 ip
+$hostname = "tcp:skugbj1ioa.database.windows.net"; //서버 ip
 $username = "decinc@skugbj1ioa";  //db접근 user
 $password = "dbswlstn1!";  //db접근 user password
 $dbName = "decinc";  //odbc dns명
  
-$mscon=mssql_connect($hostname,$username,$password) or DIE("DATABASE FAILED TO RESPOND."); 
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
 
-
-mssql_select_db($dbName,$mscon) or DIE("Table unavailable"); 
-
-
+if($conn === false){
+    die(print_r(sqlsrv_errors()));
+}
 
 //mssql_close($mscon);
 ?>
