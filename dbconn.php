227abf1cbbe87ec3c54ade9c7b6d1dfefb86af7e
Jinsoo Yoon <decinc@naver.com>
2013-04-01 00:28:03 +0900

    ;

--- dbconn.php
+++ dbconn.php
@@ -10,11 +10,12 @@ $mscon=mssql_connect($hostname,$username,$password) or DIE("DATABASE FAILED TO R
 mssql_select_db($dbName,$mscon) or DIE("Table unavailable"); 
 echo mssql_get_last_message();
 
-$q = mssql_query("select * from user");
+$q = mssql_query("select * from [dbo].[user]");
 echo mssql_get_last_message();
 
 echo $q;
 while($res = mssql_fetch_array($q)){
+	echo "loop";
 	print_r($res);
 
 }
