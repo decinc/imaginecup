20fcd026af4cce0d83720e64ee34e1a2fb00b66e
Jinsoo Yoon <decinc@naver.com>
2013-04-01 00:33:09 +0900

    ;

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include "dbconn.php";
 
  $r = mssql_query("select * from user");
 
- while($row = mssql_fetch_array($r)){
-	print_r($row);
- }
+// while($row = mssql_fetch_array($r)){
+//	print_r($row);
+// }
  echo "hello world!";
 
 ?>
