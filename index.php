08596471a61d2f1dadad469bd570c7b91eacac84
Jinsoo Yoon <decinc@naver.com>
2013-04-13 15:29:22 +0900

    asd

--- index.php
+++ index.php
@@ -1,49 +1,11 @@
 <?
-require_once "dbconn.php";
-
-$tsql = "SELECT * FROM [user]";
-$stmt = sqlsrv_query($conn, $tsql);
-   if ($stmt === false)
-   {
-     FatalError("Failed to query test table: ".$tsql);
-   }
-   else
-   {
-      while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_NUMERIC))
-      {
-		 // print_r($row);
-         //echo "Col1: ".$row[0]."\n";
-
-      }
-                                
-      sqlsrv_free_stmt($stmt);
-   }
-
-   sqlsrv_close($conn);
-
-function FatalError($errorMsg)
-{
-    Handle_Errors();
-    die($errorMsg."\n");
-}
-function Handle_Errors()
-{
-    $errors = sqlsrv_errors(SQLSRV_ERR_ERRORS);
-	print_r($errors);
-    $count = count($errors);
-    if($count == 0)
-    {
-       $errors = sqlsrv_errors(SQLSRV_ERR_ALL);
-       $count = count($errors);
-    }
-    if($count > 0)
-    {
-      for($i = 0; $i < $count; $i++)
-      {
-         print_r($errors[$i]);
-      }
-    }
-}
+@session_start();
+$is_login = false;
+if($_SESSION['id'])
+	$is_login = true;
+?>
+<?
+if(!$is_login){
 ?>
 <html>
 <head>
@@ -92,8 +54,14 @@ function Handle_Errors()
 			function okClicked () {
 
 				//ajax
-				document.title = document.getElementById ("xlInput").value;
-				closeDialog ();
+
+				$.post('login.php',{id:document.getElementById ("xlId").value, pw : document.getElementById ("xlPw").value},function(dat){
+					if(dat == "SUCCESS"){
+						closeDialog();
+					}else{
+						alert('login fail');
+					}
+				});
 				};
 function init(){
 		$('#windowTitleDialog').modal('show'); 
@@ -139,3 +107,21 @@ function init(){
 
 </body>
 </html>
+
+<?}else{?>
+//로그인했을때 코드
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?}?>
